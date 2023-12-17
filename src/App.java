b658f34e7c3bbf39106065f0f9b2ddbf181bc320
AliiAnr <alianur043@gmail.com>
2023-12-17 16:55:48 +0800

    temp

--- src/App.java
+++ src/App.java
@@ -1,6 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
         // ChessContainer chessContainer = new ChessContainer();
-        ChessContainerOffline chess = new ChessContainerOffline();
+        // ChessContainerOffline chessBoardOffline = new ChessContainerOffline();
+        Choose choose = new Choose();
     }
 }
